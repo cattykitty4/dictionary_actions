b440acb4327da4a8d9e61a6db8610d3e5dbcb7d8
cattykitty4 <aspectmastezzima@gmail.com>
2025-01-20 19:05:42 +1100

    Update code.py

--- keys_is_values/code.py
+++ keys_is_values/code.py
@@ -1,5 +1,5 @@
 def listConverter(user: [list]) -> [dict]:
-    dct = {}
+    dct = {1: 'one', 4: 'four'}
     return {element: dct.get(element, element) for element in user}
 
 # This code is a simple converter from list into dictionary. Every element from user's input convert into {key: value}
